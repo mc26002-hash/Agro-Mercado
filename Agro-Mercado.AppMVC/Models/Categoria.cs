ad01947e909e003969a6c91816497bcdb2c237cf
Jose Angel Mate Carias <mc26002@esfe.agape.edu.sv>
2026-03-17 19:52:59 -0600

    Tarea: Implementar validaciones en el modelo Categoria

--- Agro-Mercado.AppMVC/Models/Categoria.cs
+++ Agro-Mercado.AppMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Agro_Mercado.AppMVC.Models;
 
@@ -7,8 +8,11 @@ public partial class Categoria
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre de la categoría es obligatorio")]
+    [StringLength(25, ErrorMessage = "El nombre no puede superar los 25 caracteres")]
     public string Nombre { get; set; } = null!;
 
+    [StringLength(50, ErrorMessage = "La descripción no puede superar los 50 caracteres")]
     public string? Descripcion { get; set; }
 
     public bool Estado { get; set; }
