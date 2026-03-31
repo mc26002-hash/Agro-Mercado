00eae1dd764f4dfc66e2235e2399484cb4751389
WILLIAM ERNESTO OJEDA GOMEZ <williamernestoojedagomez4@gmail.com>
2026-03-31 06:50:31 -0600

    Tarea: Programar formulario de ajuste de inventario

--- Agro-Mercado.AppMVC/Migrations/20260331025604_AgregarMovimientosInventario.Designer.cs
+++ Agro-Mercado.AppMVC/Migrations/20260331025604_AgregarMovimientosInventario.Designer.cs
@@ -4,6 +4,7 @@ using Agro_Mercado.AppMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Agro_Mercado.AppMVC.Migrations
 {
     [DbContext(typeof(AgroMercadoSprintContext))]
-    partial class AgroMercadoSprintContextModelSnapshot : ModelSnapshot
+    [Migration("20260331025604_AgregarMovimientosInventario")]
+    partial class AgregarMovimientosInventario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
